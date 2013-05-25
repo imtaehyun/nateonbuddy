ac3e6749812197cf1a7d202f224e02b13423c88d
Taehyun Im <imtaehyun@me.com>
2013-05-24 23:44:57 -0400

    change config

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ var config = {
 	appKey: "fcb1a133-a4c7-3262-bf10-4e17a2789584",
     clientId: "0923817c-8d1d-394a-ab98-827d7376d636",
 	clientSecret: "fdcbc733-2e96-3923-957b-0f481ce70cd8",
-	redirectUri: "http://nateon.nezz.c9.io/auth",
+	redirectUri: "http://nateon.herokuapp.com/auth",
     scope: "nateon/note"
 }
 
